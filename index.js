ced14c737b4f559811ba948899dda14416b85c27
Pedro Henrique <pedronaame00@gmail.com>
2024-06-21 13:01:26 -0300

    Adiciona o index.html e conclui o código javascript

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-console.log("hello world")
+var nome = prompt("Nome do herói")
+var experiecia = prompt("Experiecia do herói")
+var nivel = "ferro"
+
+if (experiecia <= 1000) {
+    nivel = "Ferro"
+    window.alert("o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else if(experiecia >= 1001 && experiecia <= 2000) {
+    nivel = "Bronze"
+    window.alert("o herói de nome " + nome + "esta no nivel " + nivel)
+}   
+else if(experiecia >= 2001 && experiecia <= 5000) {
+    nivel = "Prata"
+    window.alert("o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else if(experiecia >= 5001 && experiecia <= 7000) {
+    nivel = "Ouro"
+    window.alert("o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else if(experiecia >= 7001 && experiecia <= 8000) {
+    nivel = "Platina"
+    window.alert("o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else if(experiecia >= 8001 && experiecia <= 9000) {
+    nivel = "Ascendente"
+    window.alert("o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else if (experiecia >= 9001 && experiecia <= 10000) {
+    nivel = "Imortal"
+    window.alert(" o herói de nome " + nome + " esta no nivel " + nivel)
+}
+else {
+    nivel = "Radiante"
+    window.alert(" o herói de nome " + nome + " esta no nivel " + nivel)
+}
